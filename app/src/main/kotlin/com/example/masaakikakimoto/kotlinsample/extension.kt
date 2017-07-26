33fda50254315872de28b621ae2570b9d50f8c7c
Masaaki Kakimoto <karamage.umauma@gmail.com>
2017-07-26 21:59:38 +0900

    DataBindingの準備

--- app/src/main/kotlin/com/example/masaakikakimoto/kotlinsample/extension.kt
+++ app/src/main/kotlin/com/example/masaakikakimoto/kotlinsample/extension.kt
@@ -1,9 +1,12 @@
 package com.example.masaakikakimoto.kotlinsample
 
 import android.content.Context
+import android.databinding.BindingAdapter
 import android.support.annotation.IdRes
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
+import com.bumptech.glide.Glide
 
 /**
  * Created by masaakikakimoto on 2017/07/22.
@@ -15,4 +18,9 @@ fun <T:View> View.bindView(@IdRes id: Int): Lazy<T> = lazy {
 
 fun Context.toast(message: String, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, message, duration).show()
+}
+
+@BindingAdapter("bind:imageUrl")
+fun ImageView.loadImage(url: String) {
+    Glide.with(context).load(url).into(this)
 }
